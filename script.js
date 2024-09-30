49c4bd87a7269758f406af555f99f4b61b7dfb79
Carlos Daniel Corrales <carlox59681@gmail.com>
2024-09-30 00:02:13 -0500

    interfaz mejorada

--- script.js
+++ script.js
@@ -20,6 +20,16 @@ function handleFileUpload(event) {
 		// convertir el txt en matriz
 		const matrix = parseFileUpload(content)
 		console.log(matrix)
+
+		// renderizar la matriz en el DOM
+		renderMatrix(matrix)
+
+		// Cambiar el tamaño del header para ajustar la tabla
+        document.querySelector('.header-container').classList.add('encoger');
+		document.querySelector('.header--title-container').classList.add('encoger')
+		document.querySelector('h1')
+		document.querySelector('.header--info').classList.add('invisible')
+        document.querySelector('.form-container').classList.add('encoger');
 	}
 
 	// iniciar la lectura del archivo
@@ -30,4 +40,40 @@ function handleFileUpload(event) {
 function parseFileUpload(content) {
 	// Divide el contenido del archivo por líneas y luego por espacios
     return content.trim().split('\n').map(line => line.split(' ').map(Number));
+}
+
+// función para renderizar la matriz en el DOM
+function renderMatrix(matrix) {
+	// Traer el contenedor donde va a ir el mapa
+	const mapContainer = document.getElementById('mapContainer');
+	
+	// limpiar el DOM por si hay algo ya renderizado
+	mapContainer.innerHTML = ''
+
+	// crear tabla
+	const table = document.createElement('table');
+
+	// iterar sobre la fila de la matriz
+	matrix.forEach(row => {
+		// Crear nueva fila
+		const tr = document.createElement('tr');
+
+		// iterar sobre las elementos de cada fila
+		row.forEach(element => {
+			// Crear el elemento
+			const td = document.createElement('td');
+
+			// Asignar clase de CSS dependiendo el valor de la celda
+			td.classList.add(`elemento-${element}`)
+
+			// añadir el elemento a la fila
+			tr.appendChild(td);
+		})
+
+		// añadir la fila a la tabla
+		table.appendChild(tr);
+	})
+
+	// añadir la tabla al contenedor
+	mapContainer.appendChild(table)
 }
